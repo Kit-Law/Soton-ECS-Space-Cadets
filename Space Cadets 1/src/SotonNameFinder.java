5115458ae5793871ef2016f0a73a10730837f280
Kit <34525535+Kit-Law@users.noreply.github.com>
2019-10-03 19:53:51 +0100

    Update SotonNameFinder.java

--- Space Cadets 1/src/SotonNameFinder.java
+++ Space Cadets 1/src/SotonNameFinder.java
@@ -16,24 +16,24 @@ public class SotonNameFinder
 	public static void main(String[] arg) throws IOException
 	{
 		System.out.print("Enter Email ID: ");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));			//Retrieves the ID from the user
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	//Retrieves the ID from the user
 		
-		URL soruce = new URL("https://www.ecs.soton.ac.uk/people/" + br.readLine());		//Creates the by concatenating the user input and the main web page
+		URL soruce = new URL("https://www.ecs.soton.ac.uk/people/" + br.readLine());	//Creates the by concatenating the user input and the main web page
 		br = new BufferedReader(new InputStreamReader(soruce.openStream(), "UTF-8"));
 
-		for (int i = 0; i < 7; i++)															//Discards the useless 7 leading lines of HTML
+		for (int i = 0; i < 7; i++)							//Discards the useless 7 leading lines of HTML
 		{
 			br.readLine();
 		}
 			
-		String nameBuffer = br.readLine();													//Reads the useful line of HTML into a String
-		String name = nameBuffer.substring(11, (nameBuffer.indexOf('|') - 1));				//Extracts the useful data (name), that is located at the 11th char
+		String nameBuffer = br.readLine();						//Reads the useful line of HTML into a String
+		String name = nameBuffer.substring(11, (nameBuffer.indexOf('|') - 1));		//Extracts the useful data (name), that is located at the 11th char
 		
-		if (name.equals("People"))															//If the user input is invalid then the source URL is linking to "https://www.ecs.soton.ac.uk/people/" and name will == "people"
+		if (name.equals("People"))							//If the user input is invalid then the source URL is linking to "https://www.ecs.soton.ac.uk/people/" and name will == "people"
 		{
 			System.out.print("That Was Not A Valid ID");
 		}
-		else																				//Else the user's input was valid and the appropriate name will be output
+		else										//Else the user's input was valid and the appropriate name will be output
 		{			
 			System.out.print("This ID Belongs To: " + name);
 		}
